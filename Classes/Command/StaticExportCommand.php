548eb6e99b351723dd477f843510b59d523ee5b9
Michael Blunck <michael.blunck@webicorns.com>
2025-11-22 11:57:59 +0100

    [TASK] inital commit

---
+++ Classes/Command/StaticExportCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nimmermaer\Statica\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+class StaticExportCommand extends Command
+{
+
+}
---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'statica',
+    'description' => 'Export tables to static sql dump to provider extension',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.0.0-13.4.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Nimmermaer\\Statica\\' => 'Classes/',
+        ],
+    ],
+];
